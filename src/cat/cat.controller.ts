6489b1fe4e4dc8171e18610be9e08c8f7bedbe92
HY CHOI <hychoi@pmirnc.com>
2021-06-04 18:16:28 +0900

    service 연습 https://docs.nestjs.com/providers

--- src/cat/cat.controller.ts
+++ src/cat/cat.controller.ts
@@ -1,27 +1,30 @@
 import {Controller, Get, Query} from '@nestjs/common';
 import {query} from "express";
 import { Cat } from './dto/cat.dto';
+import {CatService} from "./cat.service";
 
 
 @Controller('cat')
 export class CatController {
 
-    private cat: Cat;
 
+    constructor(private catsService:CatService) {}
+
+
+    @Get()
+    async allCat(){
+        return this.catsService.allCat();
+    }
 
     @Get("add")
-    setCat(@Query() cat:Cat): string {
-        this.cat = cat;
+    setCat(@Query() cat:Cat ): string {
+        return this.catsService.addCat(cat) ? `${cat.name} 고양이 입력 되었습니다.` : "입력에 실패했습니다";
 
-        return `입력 되었습니다.`
     }
 
     @Get("get")
     getCat(): any {
-
-        console.log(this.cat);
-
-        return this.cat
+        return this.catsService.allCat();
     }
 
 
--- src/cat/cat.service.ts
+++ src/cat/cat.service.ts
@@ -1,6 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import {Cat} from "./dto/cat.dto";
 
 @Injectable()
 export class CatService {
+    private readonly cats:Cat[] = [];
+
+    addCat(cat: Cat):number{
+
+        this.cats.push(cat);
+
+        return this.cats.length?  1 : 0;
+
+    }
+
+    allCat():string{
+        if(this.cats.length){
+            return "현재 고양이가 없습니다."
+        }else{
+            let str : string = "";
+            for(let item of this.cats){
+                str += `[ 고양이 이름은 : ${item.name}, 나이 : ${item.age}, 종 : ${item.breed} ] / `+"\n"
+            }
+            return str
+        }
+    }
+
+
+
+
+
+
+
 
 }
